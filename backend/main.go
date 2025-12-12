b0e41ee50c3eb4885c12f60a1e3e32cd7f3ccb6b
Samu-Amy <amisanosamuele@gmail.com>
2025-12-12 17:04:34 +0100

    basic API testing

--- backend/main.go
+++ backend/main.go
@@ -2,17 +2,65 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
+//TODO: JWT in HTTP only cookies (no in local storage per evitare XSS) -> attenzione a CSRF (cross origin requests)
+
 func main() {
-	mux := http.NewServeMux()
+	router := chi.NewRouter()
 
-	mux.HandleFunc("GET /api/v1/", handleRoot)
+	// TODO: aggiungi middleware generali
 
-	http.ListenAndServe(":8080", mux)
+	// TODO: aggiungi altri url, methods, ecc.
+	router.Use(cors.Handler(cors.Options{
+
+		AllowedOrigins: []string{
+			"http://localhost:5173",
+		},
+
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+		},
+
+		AllowedHeaders: []string{
+			"Content-Type",
+		},
+
+		// TODO: continua...
+	}))
+
+	router.Get("/api/v1/", handleRoot)
+	router.Get("/api/v1/menu", getMenu)
+	router.Get("/api/v1/menu/product/{productId}", getMenuProduct)
+
+	fmt.Println("Listening on http://localhost:8080")
+	err := http.ListenAndServe(":8080", router)
+
+	if err != nil {
+		log.Println(err) // TODO: sistema
+	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+func getMenu(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(`{ "status": "ok" }`))
+}
+
+func getMenuProduct(w http.ResponseWriter, r *http.Request) {
+	productId := chi.URLParam(r, "productId")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(`{ "product":` + productId + ` }`))
 }
--- frontend/src/routes/menu/+page.svelte
+++ frontend/src/routes/menu/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
   // TODO: ricorda di fare fetch in onMount
+
+  let productId = $state<number | undefined>();
+
+  onMount(async () => {
+    const res = await fetch("http://localhost:8080/api/v1/menu/product/12", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (!res.ok) {
+      console.log("Error");
+    }
+
+    let product = await res.json();
+    productId = product.product;
+  });
+
 </script>
 
 <svelte:head>
@@ -9,4 +30,9 @@
 <div class="min-w-full min-h-screen flex flex-col justify-center items-center">
   <h1>Menu</h1>
   <p>Test page (data)</p>
+  {#if productId}
+    <p>Id prodotto: {productId}</p>
+  {:else}
+    <p>Loading...</p>
+  {/if}
 </div>
