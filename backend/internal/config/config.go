b2d5e7458e8b36dfa860650b094f968c88672803
Samu-Amy <amisanosamuele@gmail.com>
2026-04-09 18:38:52 +0200

    minor changes (comments)

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 type Config struct {
 	Addr               string
-	Env                string // "env" | "prod"
+	Env                string // "env" | "prod" | "test"
 	FrontEndURL        string
 	AllowedOriginsURLs []string
 	Db                 DbConfig
--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -26,13 +26,15 @@ export default function LoginPage() {
     }
   }
 
+  // TODO: guarda tutorial e usa shadcn-ui con TanStack Form , scarica anche zod e TanStack Query
+
   return (
     <div className="flex flex-col gap-8 justify-center align-center">
       <h1>Login Page</h1>
       {error === "oauth_failed" && <p className="p-4 rounded-md bg-red-50 text-red-600 border border-red-600">
         Errore autenticazione con Google
       </p>}
-      <Button onClick={handleGoogleLogin}>Accedi con Google</Button>
+      <Button onClick={handleGoogleLogin}>Accedi con Google</Button> // TODO: blocca se non si accettano le condizioni
     </div>
   );
 }
